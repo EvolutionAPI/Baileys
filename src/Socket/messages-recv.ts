0e08f9604d3999cc53655b32850f3a5133d3a485
Davidson Gomes <contato@agenciadgcode.com>
2024-07-22 13:16:58 -0300

    fix: Resolve 'jid is undefined' error in contacts.update
    In the 'messages-recv.ts' file, the 'jid' variable was being passed as undefined when creating a new contact object in the 'contacts.update' event. This change resolves that issue by providing a default empty string value when 'jid' is undefined.
    
    The 'id' field in the contact object will now be constructed using other available attributes such as 'from' or 'hash' when 'jid' is not present. This ensures that a valid 'id' value is always provided, avoiding any further errors.

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -394,7 +394,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			const delPicture = getBinaryNodeChild(node, 'delete')
 
 			ev.emit('contacts.update', [{
-				id: jidNormalizedUser(node?.attrs?.jid) || from || ((setPicture || delPicture)?.attrs?.hash) || from || '',
+				id: jidNormalizedUser(node?.attrs?.jid) || from || ((setPicture || delPicture)?.attrs?.hash) || '',
 				imgUrl: setPicture ? 'changed' : 'removed'
 			}])
 
